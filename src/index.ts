6b794741426c940b9c478e38fff931b89ba94638
Alexander <greenwald80@gmail.com>
2021-02-21 19:41:31 +0200

    2. Отладка и инициализация в Node.JS

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express from 'express'
 const app = express();
-const port = 5000;
+import dotenv from 'dotenv';
+dotenv.config();
+
+const port = process.env.PORT;
 app.get('/', (request, response) => {
     response.send('Hello world!');
 });
+
 app.listen(port, () => console.log(`Running on port ${port}`));
