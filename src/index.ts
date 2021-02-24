23d41cfc54dd3d99e525bc244ed9e2dbcc1b545a
Alexander <greenwald80@gmail.com>
2021-02-24 15:39:00 +0200

    5. Node.JS middleware, interceptor, http context

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
 import dotenv from 'dotenv';
 import log4js from 'log4js';
-import { createExpressServer } from 'routing-controllers';
+import { useExpressServer } from 'routing-controllers';
 import { UserController } from './controller/user-controller';
+import httpContext from 'express-http-context';
+import express, { Express } from 'express';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 const logger = log4js.getLogger();
 logger.level = process.env.LOG_LEVEL;
 
-const app = createExpressServer({
+const app: Express = express();
+app.use(bodyParser.json());
+app.use(httpContext.middleware);
+useExpressServer(app, {
   controllers: [UserController] // we specify controllers we want to use
 });
+
+app.use((req, res, next) => {
+  httpContext.ns.bindEmitter(req);
+  httpContext.ns.bindEmitter(res);
+});
+
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -1,9 +1,14 @@
+import httpContext from 'express-http-context';
+
 export function loggingBefore (request: any, response: any, next?: (err?: any) => any): any {
   console.log('do something Before...');
+  console.log('set traceId = 123');
+  httpContext.set('traceId', 123);
   next();
 }
 
 export function loggingAfter ({ request, response, next }: { request: any; response: any; next?: (err?: any) => any; }): any {
   console.log('do something After...');
+  console.log(`traceId = ${httpContext.get('traceId')}`);
   next();
 }
