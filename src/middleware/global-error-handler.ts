36c42b76e0784db9018200902e1e2bfa9acdd71e
Alexander <greenwald80@gmail.com>
2021-02-26 10:33:18 +0200

    modified GlobalErrorHandler for status 400 in response

--- src/middleware/global-error-handler.ts
+++ src/middleware/global-error-handler.ts
@@ -3,7 +3,8 @@ import { ExpressErrorMiddlewareInterface, Middleware } from 'routing-controllers
 @Middleware({ type: 'after' })
 export class GlobalErrorHandler implements ExpressErrorMiddlewareInterface {
   error (error: any, request: any, response: any, next: () => any) {
-    response.send({ ERROR: error });
+    // response.send({ ERROR: error });// пропускает только со статусом 200 в ответе (если не пройдет валидацию, не пропустит)
+    response.status(error.statusCode || error.httpCode).json(error);// работает также со статусом 400, отображает причину ошибки
     next();
   }
 }
