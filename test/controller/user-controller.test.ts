1860a01e4af02ca417743e01eb095fa60dac10a3
Alexander <greenwald80@gmail.com>
2021-02-26 14:02:10 +0200

    8. Добавляем Unit тесты на Jest + Supertest

--- test/controller/user-controller.test.ts
+++ test/controller/user-controller.test.ts
@@ -1,3 +1,8 @@
+import bodyParser from 'body-parser';
+import express from 'express';
+import request from 'supertest';
+import { useExpressServer } from 'routing-controllers';
+import { GlobalErrorHandler } from '../../src/middleware/global-error-handler';
 import { UserController } from '../../src/controller/user-controller';
 import { Info } from '../../src/model/info';
 
@@ -6,6 +11,7 @@ describe('UserController', () => {
     jest.restoreAllMocks();
   });
 
+  // using jest
   it('postOne', () => {
     const userController = new UserController();
     const testBody = {
@@ -14,5 +20,31 @@ describe('UserController', () => {
     };
     const res = userController.postOne(1, testBody as Info);
     expect(res); // .toBeUndefined();
+  });
+
+  // using supertest
+  let server;
+  beforeAll(async () => {
+    server = express();
+    server.use(bodyParser.json());
+    useExpressServer(server, {
+      controllers: [UserController], // we specify controllers we want to use
+      middlewares: [GlobalErrorHandler],
+      defaultErrorHandler: false
+    });
+  });
+  // тестирую как api endpoint
+  it('postOne with validations', done => {
+    request(server)
+      .post('/users/1')
+      .send({
+        country: 'Israel',
+        city: 'Haifa'
+      } as Info)
+      .expect(204)
+      .end((err, res) => {
+        if (err) throw new Error(JSON.stringify(res.body));
+        done();
+      });
   });
 });
